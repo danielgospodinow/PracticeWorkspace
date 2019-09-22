eb35d849999aa9c8e3a0686d29a7b4c399c45579
danielgospodinow <danielgospodinow@gmail.com>
2019-09-22 21:20:35 +0300

    Add javadoc and slightly refactor

--- c++/dsp2k19/DataStructures/data_structures/array_list/array_list.hpp
+++ c++/dsp2k19/DataStructures/data_structures/array_list/array_list.hpp
@@ -12,17 +12,21 @@ public:
 
     explicit ArrayList(int capacity);
 
-    void add(T item);
+    void add(const T &item);
 
-    T get(int index);
+    void insert(const T &item, int index);
 
-    void insert(T item, int index);
+    void remove(const T &item);
 
-    void remove(int index);
+    void removeAt(int index);
 
-    bool contains(T item);
+    T get(int index) const;
 
-    void print();
+    bool contains(const T &item) const;
+
+    int size() const;
+
+    void print() const;
 
 private:
     void resize();
@@ -33,88 +37,184 @@ private:
 };
 
 template<typename T>
-ArrayList<T>::ArrayList() : ArrayList(8) {}
-
-template<typename T>
-ArrayList<T>::ArrayList(int capacity) : _array(nullptr), _capacity(capacity), _size(0) {
-    _array = new T[capacity];
+ArrayList<T>::ArrayList(const int capacity) :
+        _array(nullptr),
+        _capacity(capacity),
+        _size(0) {
+    // Initialize all class variables.
+    _array = new T[_capacity];
 }
 
 template<typename T>
-void ArrayList<T>::add(T item) {
+ArrayList<T>::ArrayList() : ArrayList(8) {
+    // If no capacity is specified, use 8 as a default initial value
+    // and delegate the initialization to the constructor above.
+}
+
+/**
+ * Add a new item to the data structure.
+ * @tparam T type
+ * @param item item to add
+ */
+template<typename T>
+void ArrayList<T>::add(const T &item) {
+    // Check if there's a need to make additional space.
     if (_size >= _capacity) {
         resize();
     }
 
+    // Add the item to the data structure and increase the size by 1.
     _array[_size++] = item;
 }
 
+/**
+ * Finds and returns an element by its index.
+ *
+ * @tparam T type
+ * @param index index of the required element.
+ * @return the element required
+ */
 template<typename T>
-T ArrayList<T>::get(int index) {
+T ArrayList<T>::get(const int index) const {
     if (index < 0 || index >= _size) {
+        // If the index is out of bounds, throw an exception.
         throw std::invalid_argument("Index out of bounds");
+    } else {
+        // Retrieve the element from the specified index.
+        return _array[index];
     }
-
-    return _array[index];
 }
 
+/**
+ * Insert an element to the data structure at a specified index.
+ *
+ * @tparam T type
+ * @param item item to insert
+ * @param index index at which to insert
+ */
 template<typename T>
-void ArrayList<T>::insert(T item, int index) {
+void ArrayList<T>::insert(const T &item, const int index) {
     if (index < 0 || index >= _size) {
-        return;
+        // If the index is out of bounds, throw an exception.
+        throw std::invalid_argument("Index out of bounds");
     } else {
+        // Check if there's a need to make additional space.
         if (_size >= _capacity) {
             resize();
         }
 
+        // Increase the size of the data structure.
+        // Note: It's important to do this here, before the for-loop
+        // below, because the loop needs to shift some elements with one
+        // position to the right.
         ++_size;
 
+        // Shift all elements from the end down to the provided index
+        // with one position to the right.
         for (int i = _size - 1; i > index; --i) {
             _array[i] = _array[i - 1];
         }
+
+        // Assign the new element.
         _array[index] = item;
     }
 }
 
+/**
+ * Remove all elements from the data structure which match
+ * the item provided.
+ *
+ * @tparam T type
+ * @param item item to be removed
+ */
 template<typename T>
-void ArrayList<T>::remove(int index) {
+void ArrayList<T>::remove(const T &item) {
+    // Iterate through all elements and remove those
+    // who are equal to the one provided.
+    for (int i = 0; i < _size; ++i) {
+        if (_array[i] == item) {
+            removeAt(i);
+        }
+    }
+}
+
+/**
+ * Remove an element from a specified index.
+ *
+ * @tparam T type
+ * @param index index of an element
+ */
+template<typename T>
+void ArrayList<T>::removeAt(const int index) {
     if (index < 0 || index >= _size) {
-        return;
+        // If the index is out of bounds, throw an exception.
+        throw std::invalid_argument("Index out of bounds");
     } else {
+        // Shift all elements from the provided index up to the last one
+        // with one position to the left.
         for (int i = index; i < _size - 1; ++i) {
             _array[i] = _array[i + 1];
         }
 
+        // Reduce the size of the data structure.
         --_size;
     }
 }
 
+/**
+ * Double the size of the data structure.
+ *
+ * @tparam T type
+ */
 template<typename T>
 void ArrayList<T>::resize() {
-    T *newStorage = new T[_capacity *= 2];
+    // Create the new bigger array with double the size of the old one.
+    T *const newStorage = new T[_capacity *= 2];
 
+    // Copy all the elements from the old array to the new one.
     for (int i = 0; i < _size; ++i) {
         newStorage[i] = _array[i];
     }
 
+    // Make the bigger array to be the current array of
+    // the data structure and clear the old one.
     T *oldStorage = _array;
     _array = newStorage;
     delete[] oldStorage;
 }
 
+/**
+ * Checks whether an element is present in the data structure.
+ *
+ * @tparam T type
+ * @param item item to check for
+ * @return true if the item is present, false otherwise
+ */
 template<typename T>
-bool ArrayList<T>::contains(T item) {
+bool ArrayList<T>::contains(const T &item) const {
+    // Iterate through all elements and return 'true'
+    // if an element matches the provided one.
     for (int i = 0; i < _size; ++i) {
         if (_array[i] == item) {
             return true;
         }
     }
 
+    // Return 'false' otherwise.
     return false;
 }
 
 template<typename T>
-void ArrayList<T>::print() {
+int ArrayList<T>::size() const {
+    // Return the size of the data structure.
+    return _size;
+}
+
+template<typename T>
+void ArrayList<T>::print() const {
+    // Iterate through all elements and print
+    // them to the console.
+
     for (int i = 0; i < _size; ++i) {
         std::cout << get(i) << " ";
     }
--- c++/dsp2k19/DataStructures/data_structures/linked_list/linked_list.hpp
+++ c++/dsp2k19/DataStructures/data_structures/linked_list/linked_list.hpp
@@ -17,62 +17,305 @@ class LinkedList {
 public:
     LinkedList();
 
-    void print();
+    void add(const T &item);
 
-    void add(T item);
+    T get(int index) const;
 
-    void remove(T item);
+    void insert(const T &item, int index);
+
+    void remove(const T &item);
+
+    void removeAt(int index);
+
+    bool contains(const T &item) const;
+
+    int size() const;
+
+    void print() const;
 
 private:
-    Node<T> *_head = nullptr;
-    Node<T> *_tail = nullptr;
+    Node<T> *_head;
+    Node<T> *_tail;
+    int _size;
 };
 
 template<typename T>
-LinkedList<T>::LinkedList() = default;
+LinkedList<T>::LinkedList():
+        _head(nullptr),
+        _tail(nullptr),
+        _size(0) {
+    // Initialize all class variables.
+}
 
+/**
+ * Add a new item to the data structure.
+ *
+ * @tparam T type
+ * @param item the new item
+ */
 template<typename T>
-void LinkedList<T>::add(T item) {
-    auto *newNode = new Node<T>(item);
+void LinkedList<T>::add(const T &item) {
+    //TODO: Make use of _tail
+
+    // Create a box which wraps the new element.
+    auto *const newNode = new Node<T>(item);
 
     if (!_head) {
+        // If the head is NULL then the data structure is empty,
+        // so make the first element (the head) be the new element.
         _head = newNode;
+
+        // Increase the size of the data structure.
+        ++_size;
     } else {
+        // If the head is not null, then we have some elements
+        // to work with.
+
+        // Create a temporary variable with which we'll iterate
+        // through all elements.
         Node<T> *iter = _head;
+
+        // Iterate through all elements util reaching the last element.
         while (iter->next) {
             iter = iter->next;
         }
+
+        // Set the new element to be current last element's next item,
+        // thus making it the new last element.
         iter->next = newNode;
+
+        // Increase the size of the data structure.
+        ++_size;
     }
 }
 
+/**
+ * Finds and returns an element by its index.
+ *
+ * @tparam T type
+ * @param index index of the required element.
+ * @return the element required
+ */
 template<typename T>
-void LinkedList<T>::remove(T item) {
-    Node<T> *iter = _head;
+T LinkedList<T>::get(const int index) const {
+    if (index < 0 || index >= _size) {
+        // If the index is out of bounds, throw an exception.
+        throw std::invalid_argument("Index out of bounds");
+    } else {
+        // Initialize an index counter and a pointer to the head element.
+        int indexCounter = 0;
+        Node<T> *iter = _head;
 
-    if (iter) {
-        Node<T> *prevElem = iter;
+        // Move the pointer to point to the next element until we
+        // reach the desired index.
+        while (indexCounter++ < index) {
+            iter = iter->next;
+        }
 
-        while (iter) {
-            if (iter->elem == item) {
-                if (iter == _head) {
-                    _head = _head->next;
-                }
+        // Then return that exact item.
+        return iter->elem;
+    }
+}
 
-                prevElem->next = iter->next;
-                delete iter;
+/**
+ * Insert an element to the data structure at a specified index.
+ *
+ * @tparam T type
+ * @param item item to insert
+ * @param index index at which to insert
+ */
+template<typename T>
+void LinkedList<T>::insert(const T &item, const int index) {
+    if (index < 0 || index >= _size) {
+        // If the index is out of bounds, throw an exception.
+        throw std::invalid_argument("Index out of bounds");
+    } else {
+        // Create a box which wraps the new element.
+        auto *const newNode = new Node<T>(item);
 
-                iter = prevElem->next;
-            }
+        // Initialize an index counter and two pointers to
+        // track two consecutive elements.
+        int indexCounter = 0;
+        Node<T> *prev = nullptr;
+        Node<T> *iter = _head;
 
-            prevElem = iter;
-            iter = prevElem->next;
+        // While we haven't reached the desired index,
+        // move both element pointers to the right.
+        while (indexCounter++ < index) {
+            prev = iter;
+            iter = iter->next;
+        }
+
+        if (prev && iter) {
+            // If we have both pointers pointing to an element,
+            // then we're somewhere after the first element in the
+            // data structure, so make the current element's previous
+            // point to the new element and make the new element point
+            // to the current one.
+            prev->next = newNode;
+            newNode->next = iter;
+
+            // Increase the data structure's size.
+            ++_size;
+        } else if (iter) {
+            // If only the pointer to the current element is
+            // not null, then we're at the first element of the
+            // data structure (the head), so make the new element
+            // be the head and point it to the old head element.
+            _head = newNode;
+            _head->next = iter;
+
+            // Increase the data structure's size.
+            ++_size;
         }
     }
 }
 
+/**
+ * Remove all elements from the data structure which match
+ * the item provided.
+ *
+ * @tparam T type
+ * @param item item to be removed
+ */
 template<typename T>
-void LinkedList<T>::print() {
+void LinkedList<T>::remove(const T &item) {
+    // Initialize pointers to two consecutive elements
+    // with the second one being the first element.
+    Node<T> *prev = nullptr;
+    Node<T> *curr = _head;
+
+    // Iterate through all two consecutive elements while the second of the two
+    // consecutive elements is not null.
+    while (curr) {
+
+        // If the current element's value is equal to the
+        // value we're looking for.
+        if (curr->elem == item) {
+
+            // If the current element is the first element in the data structure,
+            // then move the head with one position to the right.
+            if (curr == _head) {
+                _head = _head->next;
+            }
+
+            // If the current element's previous element is not null,
+            // move it to the right.
+            if (prev) {
+                prev->next = curr->next;
+            }
+
+            // Connect the current element's previous with the next element
+            // in the data structure and delete the current one.
+            Node<T> *temp = curr;
+            curr = curr->next;
+            delete temp;
+
+            // Reduce the size of the data structure.
+            --_size;
+        } else {
+            // If the current element's value is not equal
+            // to the value we're looking for, then just move both
+            // elements with one position to the right.
+            prev = curr;
+            curr = curr->next;
+        }
+    }
+}
+
+/**
+ * Remove an element from a specified index.
+ *
+ * @tparam T type
+ * @param index index of an element
+ */
+template<typename T>
+void LinkedList<T>::removeAt(const int index) {
+    if (index < 0 || index >= _size) {
+        // If the index is out of bounds, throw an exception.
+        throw std::invalid_argument("Index out of bounds");
+    } else {
+        // Create an index counter and two pointers which
+        // we'll use to keep track of every two consecutive
+        // elements in the data structure.
+        int indexCounter = 0;
+        Node<T> *prev = nullptr;
+        Node<T> *iter = _head;
+
+        // Iterate through elements util
+        // the desired element index is reached.
+        while (indexCounter++ < index) {
+            prev = iter;
+            iter = iter->next;
+        }
+
+        if (prev && iter) {
+            // If both consecutive elements exist this means
+            // that we're surely somewhere after the first
+            // element.
+
+            // Break the connection between the two consecutive
+            // elements and make the first one point to the
+            // second one's next element, thus preparing the
+            // second element for deletion.
+            prev->next = iter->next;
+
+            // Reduce the size of the data structure by 1 and
+            // delete the second of the two consecutive elements.
+            --_size;
+            delete iter;
+        } else if (iter) {
+            // If only the second of the two consecutive elements
+            // exist, this means that we're at the beginning of
+            // the data structure. In this case we need to move
+            // the head with one position.
+            _head = _head->next;
+
+            // Reduce the size of the data structure by 1 and
+            // delete the second of the two consecutive elements.
+            --_size;
+            delete iter;
+        }
+    }
+}
+
+/**
+ * Checks whether an element is present in the data structure.
+ *
+ * @tparam T type
+ * @param item item to check for
+ * @return true if the item is present, false otherwise
+ */
+template<typename T>
+bool LinkedList<T>::contains(const T &item) const {
+    // Initialize a pointer to the head element.
+    Node<T> *iter = _head;
+
+    while (iter) {
+        // While the current element exists, check whether its element
+        // is equal to the one we're looking for and return true if
+        // that's the case.
+        if (iter->elem == item) {
+            return true;
+        }
+
+        // If we don't match, move the current element pointer to the
+        // next one.
+        iter = iter->next;
+    }
+
+    // Return false if we haven't got any matches.
+    return false;
+}
+
+template<typename T>
+int LinkedList<T>::size() const {
+    // Return the size of the data structure.
+    return _size;
+}
+
+template<typename T>
+void LinkedList<T>::print() const {
     Node<T> *iter = _head;
 
     while (iter) {
