dc5474221a15db2b3f33cf8ce1507a9715a2bf9e
danielgospodinow <danielgospodinow@gmail.com>
2019-11-03 03:39:19 -0800

    Add travelling salesman genetic algorithm start project

---
+++ ai/genetic-algorithms-travelling-salesman/src/main/java/com/danielgospodinow/ai/genetic/algorithms/travelling/salesman/Main.java
@@ -0,0 +1,8 @@
+package com.danielgospodinow.ai.genetic.algorithms.travelling.salesman;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Yooo, genetics");
+    }
+}
