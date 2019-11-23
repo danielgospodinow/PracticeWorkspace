f3c3bb336f69d7d1dec676f20b2a6859fd66b09b
danielgospodinow <danielgospodinow@gmail.com>
2019-11-23 12:17:54 +0200

    Make choosing of first player be from the standart input

--- ai/alpha-beta-pruning-tic-tac-toe/src/main/java/com/danielgospodinow/ai/minimax/alpha/beta/pruning/tic/tac/toe/Main.java
+++ ai/alpha-beta-pruning-tic-tac-toe/src/main/java/com/danielgospodinow/ai/minimax/alpha/beta/pruning/tic/tac/toe/Main.java
@@ -1,9 +1,16 @@
 package com.danielgospodinow.ai.minimax.alpha.beta.pruning.tic.tac.toe;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         TicTacToeGame ticTacToeGame = new TicTacToeGame();
-        ticTacToeGame.start(false);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Do you want to start first? Or you'll be a gentleman and let the AI go first?");
+        boolean humanFirst = scanner.nextLine().contains("y");
+
+        ticTacToeGame.start(humanFirst);
     }
 }
